a828cc8f522719e4e03584e88915dbf32df4d1ec
mrkaashee <mrkaashee@gmail.com>
2026-03-18 20:42:33 +0500

    Update AppHeader.vue

--- app/components/app/AppHeader.vue
+++ app/components/app/AppHeader.vue
@@ -5,8 +5,19 @@ const { links } = useNavigation()
 <template>
   <UHeader title="Tools">
     <UNavigationMenu :items="links" />
+
     <template #right>
       <UColorModeButton />
+      <UButton
+        icon="i-simple-icons-github"
+        color="neutral"
+        variant="ghost"
+        to="https://github.com/mrkaashee/tools"
+        target="_blank" />
+    </template>
+
+    <template #body>
+      <UNavigationMenu :items="links" orientation="vertical" />
     </template>
   </UHeader>
 </template>
